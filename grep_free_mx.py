bf33bbdee3cc19c31ef7d2b1429d725f6a205ecc
CalerWang <kai0200@gmail.com>
2021-05-02 10:57:38 +0800

    change grep_free_mx.py

--- grep_free_mx.py
+++ grep_free_mx.py
@@ -5,8 +5,8 @@
 python g.py <Email> [1 2 3]
 
 g.py user@domain.com 搜索当天日志
-g.py user@domain.com -d 2 两天以内的
-g.py user@domain.com -l a.log b.log c.log 搜索多个日志文件
+g.py user@domain.com -d n 两天以内的
+g.py user@domain.com -f a.log b.log c.log 搜索多个日志文件
 """
 
 import argparse
@@ -26,49 +26,36 @@ def search_record(record, logfile):
 # 4FVfGH0brJz4wXZ /home/caler/wk/2021.04.28.log
 def get_queueid_sender(sender, log):
     """ Get email queue id from log for sender return list"""
-    if sender in log:
-        return log.split(',')[0].split(': ')[-2]
-    else:
-        return ''
+    return log.split(',')[0].split(': ')[-2] if sender in log else 'Null'
 
 
 def convert_time(time_str):
+    """ Return time of the log """
     time = re.search(r'\w{3}\ \d{2}\ \d{2}:\d{2}:\d{2}', time_str)
-    if time:
-        return parse(time.group()).strftime("%Y-%m-%d %H:%M:%S")
-    else:
-        return ''
+    return parse(
+        time.group()).strftime("%Y-%m-%d %H:%M:%S") if time else 'Null'
 
 
 def get_ipaddress(record):
     ip = re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', record)
-    if ip:
-        return ip.group()
-    else:
-        return ''
+    return ip.group() if ip else 'Null'
 
 
 def get_email_address(record):
     email = re.search(r'[\w\.-]+@[\w\.-]+', record)
-    if email:
-        return email.group()
-    else:
-        return ''
+    return email.group() if email else 'Null'
 
 
 def get_status(record):
     status = re.search(r'\d{3}\ \d\.\d\.\d\ \w+', record)
-    if status:
-        return status.group()
-    else:
-        return ''
+    return status.group() if status else 'Null'
 
 
 def get_today_logfile():
     return datetime.datetime.today().strftime("%Y.%m.%d.log")
 
 
-# search 搜索指定的文件（单独一个）
+# search 搜索一个文件
 def search(email, logfile):
     records = search_record(email, logfile)
     for record in records:
@@ -88,44 +75,46 @@ def search(email, logfile):
             to = 'Null'
             status = 'Null'
 
-        print("%s-<%s> - Time: %s IP: %s From: <%s> To: <%s> Status: %s " %
-              (logfile, email, time, ip, sender, to, status))
+        print("%s - <%s> %s Time:%s IP:%s From:<%s> To:<%s> Status:%s " %
+              (logfile, email, qid, time, ip, sender, to, status))
 
 
 def get_logfile_name(days):
-    d = []
-    d.append(datetime.datetime.today().strftime("%Y.%m.%d.log"))
+    """get n days ago logfile name """
+    file_names = []
+    file_names.append(datetime.datetime.today().strftime("%Y.%m.%d.log"))
     if days > 0:
-        for n in range(0, days):
+        for n in range(1, days + 1):
             n_day_ago = (datetime.datetime.today() -
                          datetime.timedelta(days=n))
-            d.append(n_day_ago.strftime("%Y.%m.%d.log"))
-    return d
+            file_names.append(n_day_ago.strftime("%Y.%m.%d.log"))
+    return file_names
 
 
 if __name__ == "__main__":
-    #email = "support@salesforce.com"
-    #logfile = "/home/caler/wk/2021.04.28.log"
-
+    # 解析参数
     parser = argparse.ArgumentParser(description="search email from logfile")
+    group = parser.add_mutually_exclusive_group()
     parser.add_argument("email_address", help='user@domain.com')
     parser.add_argument('--version',
                         '-v',
                         action='version',
                         version='%(prog)s version : v 0.01',
                         help='show the version')
-    parser.add_argument('--days',
-                        '-d',
-                        default=0,
-                        type=int,
-                        dest='days',
-                        help='%(prog) user@domain.com -d 1[2 3]')
     parser.add_argument(
-        '--file',
-        '-f',
-        nargs='+',
-        dest='files',
-        help='%(prog) user@domain.com -f xxxx.xx.xx.log xxxx.xx.xx.log ...')
+        '--days',
+        '-d',
+        default=0,
+        dest='days',
+        type=int,
+        #action='store_true',
+        help='%(prog) user@domain.com -d 1[2 3]')
+    # --file 支持接多个文件，使用nargs=‘+’
+    parser.add_argument('--file',
+                        '-f',
+                        nargs='+',
+                        dest='files',
+                        help='%(prog) user@domain.com -f file1 file2 ...')
     args = parser.parse_args()
 
     # main
@@ -134,11 +123,13 @@ if __name__ == "__main__":
 
     # 如果参数-f 指定了相关多个文件，增加到logfiles 列表里
     if args.files:
-        logfiles += args.files
+        #logfiles += args.files
+        logfiles = args.files if args.days == 0 else logfiles + args.files
 
     print(logfiles)
 
     for file in logfiles:
+        print("LogFile: %s" % file)
         if os.path.exists(file):
             search(args.email_address, file)
         else:
